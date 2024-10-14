7b495695f6b3936a10a8f5b3091d6f51d1196765
Aditya Jaiswal <32607172+jaiswaladi246@users.noreply.github.com>
2024-10-14 14:44:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -108,7 +108,7 @@ resource "aws_eks_node_group" "devopsshack" {
     min_size     = 3
   }
 
-  instance_types = ["t2.large"]
+  instance_types = ["t2.medium"]
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
